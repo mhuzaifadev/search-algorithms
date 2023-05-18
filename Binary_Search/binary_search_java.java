2d48c359d82c38974bee8832feca788f22911b11
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-19 00:08:44 +0500

    Create binary_search_java.java

---
+++ Binary_Search/binary_search_java.java
@@ -0,0 +1,29 @@
+// Binary Search in Java
+
+public class BinarySearch {
+    public static int binarySearch(int[] arr, int target) {
+        // Set the lower bound and upper bound
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            // Calculate the middle index
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                // Return the index if the target is found
+                return mid;
+            } else if (arr[mid] < target) {
+                // Update the lower bound if the target is in the right half
+                low = mid + 1;
+            } else {
+                // Update the upper bound if the target is in the left half
+                high = mid - 1;
+            }
+        }
+
+        // Return -1 if the target is not found
+        return -1;
+    }
+}
+
