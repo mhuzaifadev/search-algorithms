56f455a55ac4f736c9dbd49852b1bf0439e438cb
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-23 01:30:05 +0500

    Update a_search_java.java

--- A Search/a_search_java.java
+++ A Search/a_search_java.java
@@ -62,33 +62,3 @@ public class AStarSearch {
         }
     }
 }
-
-interface HeuristicFunction {
-    int calculate(int current, int goal);
-}
-
-public class BreadthFirstSearch {
-    public static void bfs(Map<Integer, List<Integer>> graph, int start) {
-        Queue<Integer> queue = new LinkedList<>();  // Create an empty queue and enqueue the starting vertex
-        Set<Integer> visited = new HashSet<>();  // Create an empty set to track visited vertices
-
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-            int vertex = queue.poll();  // Dequeue a vertex from the queue
-
-            if (!visited.contains(vertex)) {  // If the vertex has not been visited
-                visited.add(vertex);  // Mark the vertex as visited
-                processVertex(vertex);  // Process the vertex (e.g., print it, add it to a result list)
-
-                // Enqueue all unvisited neighbors of the vertex
-                for (int neighbor : graph.get(vertex)) {
-                    if (!visited.contains(neighbor)) {
-                        queue.add(neighbor);
-                    }
-                }
-            }
-        }
-    }
-}
-
