0d949fcf099d614790823c879a8daa6ad3d31204
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-24 02:37:00 +0500

    Update dijkstra's_algorithm_rlang.r

--- Dijkstra's_Algorithm/dijkstra's_algorithm_rlang.r
+++ Dijkstra's_Algorithm/dijkstra's_algorithm_rlang.r
@@ -1,57 +1,35 @@
 # Uniform Cost Search in R Language
 
-# Install the priorityqueue package if not already installed
-# install.packages("priorityqueue")
+library(pqueue)
 
-library(priorityqueue)
+dijkstra <- function(graph, start) {
+  distances <- rep(Inf, length(graph))
+  distances[start] <- 0
+  pq <- pqueue$new(order = "min")
+  pq$insert(start, 0)
 
-ucs <- function(graph, start, goal) {
-  priority_queue <- priority_queue$new(order = "min")
-  visited <- set()
-  cost_so_far <- list()
-  came_from <- list()
+  while (!pq$is_empty()) {
+    node <- pq$pop()
+    current_node <- node$elem
+    current_distance <- node$priority
 
-  priority_queue$insert(start, 0)
-  cost_so_far[[start]] <- 0
-
-  while (!priority_queue$is_empty()) {
-    node <- priority_queue$pop()
-    vertex <- node$elem
-
-    if (vertex == goal) {
-      break
+    if (current_distance > distances[current_node]) {
+      next
     }
 
-    if (!(vertex %in% visited)) {
-      visited <- union(visited, vertex)
+    neighbors <- graph[[current_node]]
 
-      neighbors <- graph[[vertex]]
-      for (neighbor in names(neighbors)) {
-        cost <- neighbors[[neighbor]]
-        new_cost <- cost_so_far[[vertex]] + cost
+    for (i in seq_along(neighbors$vertex)) {
+      neighbor_node <- neighbors$vertex[i]
+      neighbor_distance <- neighbors$distance[i]
+      distance <- current_distance + neighbor_distance
 
-        if (!(neighbor %in% names(cost_so_far)) || new_cost < cost_so_far[[neighbor]]) {
-          cost_so_far[[neighbor]] <- new_cost
-          priority_queue$insert(neighbor, new_cost)
-          came_from[[neighbor]] <- vertex
-        }
+      if (distance < distances[neighbor_node]) {
+        distances[neighbor_node] <- distance
+        pq$insert(neighbor_node, distance)
       }
     }
   }
 
-  reconstruct_path(start, goal, came_from)
-}
-
-reconstruct_path <- function(start, goal, came_from) {
-  path <- vector("list", 0)
-  current <- goal
-
-  while (current != start) {
-    path <- c(path, current)
-    current <- came_from[[current]]
-  }
-
-  path <- c(path, start)
-  path <- rev(path)
-  return(path)
+  distances
 }
