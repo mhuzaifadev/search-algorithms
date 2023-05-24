539ea4f35bf54384a7bdce94812dccb7e0d51c94
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-25 04:02:40 +0500

    Update iddfs_algorithm_csharp.cs

--- Iterative Deepening DFS Algorithm/iddfs_algorithm_csharp.cs
+++ Iterative Deepening DFS Algorithm/iddfs_algorithm_csharp.cs
@@ -2,151 +2,71 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
-public class Dijkstra
+public class IDDFS
 {
-    public static Dictionary<int, int> DijkstraAlgorithm(Dictionary<int, Dictionary<int, int>> graph, int start)
+    public static bool IDDFSAlgorithm(Dictionary<string, List<string>> graph, string start, string target, int maxDepth)
     {
-        Dictionary<int, int> distances = new Dictionary<int, int>();
-        PriorityQueue<Node> priorityQueue = new PriorityQueue<Node>();
-        HashSet<int> visited = new HashSet<int>();
-
-        // Initialize distances with infinity except for the start node
-        foreach (int node in graph.Keys)
+        for (int depth = 0; depth <= maxDepth; depth++)
         {
-            distances[node] = int.MaxValue;
-        }
-        distances[start] = 0;
-
-        priorityQueue.Enqueue(new Node(start, 0));
-
-        while (!priorityQueue.IsEmpty)
-        {
-            Node current = priorityQueue.Dequeue();
-            int currentNode = current.Node;
-
-            // Skip if the current node has already been visited
-            if (visited.Contains(currentNode))
+            HashSet<string> visited = new HashSet<string>();
+            if (DLS(graph, start, target, depth, visited))
             {
-                continue;
-            }
-
-            visited.Add(currentNode);
-
-            // Explore neighbors of the current node
-            foreach (var neighbor in graph[currentNode])
-            {
-                int neighborNode = neighbor.Key;
-                int weight = neighbor.Value;
-                int distance = distances[currentNode] + weight;
-
-                // Update distance if a shorter path is found
-                if (distance < distances[neighborNode])
-                {
-                    distances[neighborNode] = distance;
-                    priorityQueue.Enqueue(new Node(neighborNode, distance));
-                }
+                return true;
             }
         }
-
-        return distances;
+        return false;
     }
 
-    private class Node : IComparable<Node>
+    public static bool DLS(Dictionary<string, List<string>> graph, string node, string target, int depth, HashSet<string> visited)
     {
-        public int Node { get; }
-        public int Distance { get; }
-
-        public Node(int node, int distance)
+        if (depth == 0 && node == target)
         {
-            Node = node;
-            Distance = distance;
+            return true;
         }
-
-        public int CompareTo(Node other)
+        if (depth > 0)
         {
-            return Distance.CompareTo(other.Distance);
+            visited.Add(node);
+            if (graph.ContainsKey(node))
+            {
+                foreach (string neighbor in graph[node])
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        if (DLS(graph, neighbor, target, depth - 1, visited))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
         }
+        return false;
     }
 
-    private class PriorityQueue<T> where T : IComparable<T>
+    public static void Main(string[] args)
     {
-        private List<T> heap;
-
-        public PriorityQueue()
+        Dictionary<string, List<string>> graph = new Dictionary<string, List<string>>()
         {
-            heap = new List<T>();
+            { "A", new List<string> { "B", "C" } },
+            { "B", new List<string> { "D", "E" } },
+            { "C", new List<string> { "F" } },
+            { "D", new List<string> { } },
+            { "E", new List<string> { "F" } },
+            { "F", new List<string> { } }
+        };
+
+        string start = "A";
+        string target = "F";
+        int maxDepth = 3;
+
+        if (IDDFSAlgorithm(graph, start, target, maxDepth))
+        {
+            Console.WriteLine("Path exists");
         }
-
-        public void Enqueue(T item)
+        else
         {
-            heap.Add(item);
-            int i = heap.Count - 1;
-
-            while (i > 0)
-            {
-                int parent = (i - 1) / 2;
-
-                if (heap[parent].CompareTo(heap[i]) <= 0)
-                {
-                    break;
-                }
-
-                Swap(parent, i);
-                i = parent;
-            }
-        }
-
-        public T Dequeue()
-        {
-            T item = heap[0];
-            int lastIndex = heap.Count - 1;
-            heap[0] = heap[lastIndex];
-            heap.RemoveAt(lastIndex);
-
-            int i = 0;
-            lastIndex--;
-
-            while (true)
-            {
-                int leftChild = 2 * i + 1;
-
-                if (leftChild > lastIndex)
-                {
-                    break;
-                }
-
-                int rightChild = leftChild + 1;
-                int minChild = leftChild;
-
-                if (rightChild <= lastIndex && heap[rightChild].CompareTo(heap[leftChild]) < 0)
-                {
-                    minChild = rightChild;
-                }
-
-                if (heap[i].CompareTo(heap[minChild]) <= 0)
-                {
-                    break;
-                }
-
-                Swap(i, minChild);
-                i = minChild;
-            }
-
-            return item;
-        }
-
-        public bool IsEmpty
-        {
-            get { return heap.Count == 0; }
-        }
-
-        private void Swap(int i, int j)
-        {
-            T temp = heap[i];
-            heap[i] = heap[j];
-            heap[j] = temp;
+            Console.WriteLine("Path does not exist");
         }
     }
 }
