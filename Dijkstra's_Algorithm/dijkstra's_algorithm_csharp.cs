8ac3d4e29ed06883f028e694d26ac39d235a8ea7
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-24 11:18:10 +0500

    Update dijkstra's_algorithm_csharp.cs

--- Dijkstra's_Algorithm/dijkstra's_algorithm_csharp.cs
+++ Dijkstra's_Algorithm/dijkstra's_algorithm_csharp.cs
@@ -9,36 +9,43 @@ public class Dijkstra
     public static Dictionary<int, int> DijkstraAlgorithm(Dictionary<int, Dictionary<int, int>> graph, int start)
     {
         Dictionary<int, int> distances = new Dictionary<int, int>();
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<Node>();
+        HashSet<int> visited = new HashSet<int>();
+
+        // Initialize distances with infinity except for the start node
         foreach (int node in graph.Keys)
         {
             distances[node] = int.MaxValue;
         }
         distances[start] = 0;
 
-        SortedSet<Node> priorityQueue = new SortedSet<Node>();
-        priorityQueue.Add(new Node(start, 0));
+        priorityQueue.Enqueue(new Node(start, 0));
 
-        while (priorityQueue.Count > 0)
+        while (!priorityQueue.IsEmpty)
         {
-            Node current = priorityQueue.Min;
-            priorityQueue.Remove(current);
+            Node current = priorityQueue.Dequeue();
+            int currentNode = current.Node;
 
-            if (current.Distance > distances[current.Vertex])
+            // Skip if the current node has already been visited
+            if (visited.Contains(currentNode))
             {
                 continue;
             }
 
-            foreach (var neighbor in graph[current.Vertex])
+            visited.Add(currentNode);
+
+            // Explore neighbors of the current node
+            foreach (var neighbor in graph[currentNode])
             {
                 int neighborNode = neighbor.Key;
                 int weight = neighbor.Value;
-                int distance = distances[current.Vertex] + weight;
+                int distance = distances[currentNode] + weight;
 
+                // Update distance if a shorter path is found
                 if (distance < distances[neighborNode])
                 {
-                    priorityQueue.RemoveWhere(n => n.Vertex == neighborNode);
                     distances[neighborNode] = distance;
-                    priorityQueue.Add(new Node(neighborNode, distance));
+                    priorityQueue.Enqueue(new Node(neighborNode, distance));
                 }
             }
         }
@@ -48,18 +55,98 @@ public class Dijkstra
 
     private class Node : IComparable<Node>
     {
-        public int Vertex { get; }
+        public int Node { get; }
         public int Distance { get; }
 
-        public Node(int vertex, int distance)
+        public Node(int node, int distance)
         {
-            Vertex = vertex;
+            Node = node;
             Distance = distance;
         }
 
         public int CompareTo(Node other)
         {
             return Distance.CompareTo(other.Distance);
+        }
+    }
+
+    private class PriorityQueue<T> where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public PriorityQueue()
+        {
+            heap = new List<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            heap.Add(item);
+            int i = heap.Count - 1;
+
+            while (i > 0)
+            {
+                int parent = (i - 1) / 2;
+
+                if (heap[parent].CompareTo(heap[i]) <= 0)
+                {
+                    break;
+                }
+
+                Swap(parent, i);
+                i = parent;
+            }
+        }
+
+        public T Dequeue()
+        {
+            T item = heap[0];
+            int lastIndex = heap.Count - 1;
+            heap[0] = heap[lastIndex];
+            heap.RemoveAt(lastIndex);
+
+            int i = 0;
+            lastIndex--;
+
+            while (true)
+            {
+                int leftChild = 2 * i + 1;
+
+                if (leftChild > lastIndex)
+                {
+                    break;
+                }
+
+                int rightChild = leftChild + 1;
+                int minChild = leftChild;
+
+                if (rightChild <= lastIndex && heap[rightChild].CompareTo(heap[leftChild]) < 0)
+                {
+                    minChild = rightChild;
+                }
+
+                if (heap[i].CompareTo(heap[minChild]) <= 0)
+                {
+                    break;
+                }
+
+                Swap(i, minChild);
+                i = minChild;
+            }
+
+            return item;
+        }
+
+        public bool IsEmpty
+        {
+            get { return heap.Count == 0; }
+        }
+
+        private void Swap(int i, int j)
+        {
+            T temp = heap[i];
+            heap[i] = heap[j];
+            heap[j] = temp;
         }
     }
 }
