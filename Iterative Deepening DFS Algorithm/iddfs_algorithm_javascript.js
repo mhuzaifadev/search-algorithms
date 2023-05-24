1540c9ad73062f7fa1375627a43652980f7682ba
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2023-05-25 04:03:18 +0500

    Update iddfs_algorithm_javascript.js

--- Iterative Deepening DFS Algorithm/iddfs_algorithm_javascript.js
+++ Iterative Deepening DFS Algorithm/iddfs_algorithm_javascript.js
@@ -1,119 +1,50 @@
 // Uniform Cost Search in JavaScript
 
-class PriorityQueue {
-  constructor(comparator = (a, b) => a.priority < b.priority) {
-    this.heap = [];
-    this.comparator = comparator;
-  }
-
-  enqueue(element, priority) {
-    const node = { element, priority };
-    this.heap.push(node);
-    this.bubbleUp(this.heap.length - 1);
-  }
-
-  dequeue() {
-    if (this.isEmpty()) {
-      return null;
-    }
-    this.swap(0, this.heap.length - 1);
-    const node = this.heap.pop();
-    this.bubbleDown(0);
-    return node.element;
-  }
-
-  isEmpty() {
-    return this.heap.length === 0;
-  }
-
-  bubbleUp(index) {
-    const parentIndex = Math.floor((index - 1) / 2);
-    if (index > 0 && this.comparator(this.heap[index], this.heap[parentIndex])) {
-      this.swap(index, parentIndex);
-      this.bubbleUp(parentIndex);
+function iddfs(graph, start, target, maxDepth) {
+  for (let depth = 0; depth <= maxDepth; depth++) {
+    let visited = new Set();
+    if (dls(graph, start, target, depth, visited)) {
+      return true;
     }
   }
-
-  bubbleDown(index) {
-    const leftChildIndex = 2 * index + 1;
-    const rightChildIndex = 2 * index + 2;
-    let smallestChildIndex = index;
-
-    if (
-      leftChildIndex < this.heap.length &&
-      this.comparator(this.heap[leftChildIndex], this.heap[smallestChildIndex])
-    ) {
-      smallestChildIndex = leftChildIndex;
-    }
-
-    if (
-      rightChildIndex < this.heap.length &&
-      this.comparator(this.heap[rightChildIndex], this.heap[smallestChildIndex])
-    ) {
-      smallestChildIndex = rightChildIndex;
-    }
-
-    if (smallestChildIndex !== index) {
-      this.swap(index, smallestChildIndex);
-      this.bubbleDown(smallestChildIndex);
-    }
-  }
-
-  swap(index1, index2) {
-    [this.heap[index1], this.heap[index2]] = [this.heap[index2], this.heap[index1]];
-  }
+  return false;
 }
 
-function dijkstra(graph, start) {
-  const distances = {};
-  const priorityQueue = new PriorityQueue((a, b) => a.distance < b.distance);
-  const visited = new Set();
-
-  // Initialize distances with infinity except for the start node
-  for (const node in graph) {
-    distances[node] = Infinity;
+function dls(graph, node, target, depth, visited) {
+  if (depth === 0 && node === target) {
+    return true;
   }
-  distances[start] = 0;
-
-  priorityQueue.enqueue({ node: start, distance: 0 });
-
-  while (!priorityQueue.isEmpty()) {
-    const current = priorityQueue.dequeue();
-    const currentNode = current.node;
-
-    // Skip if the current node has already been visited
-    if (visited.has(currentNode)) {
-      continue;
-    }
-
-    visited.add(currentNode);
-
-    // Explore neighbors of the current node
-    for (const neighbor in graph[currentNode]) {
-      const neighborNode = parseInt(neighbor);
-      const weight = graph[currentNode][neighbor];
-      const distance = distances[currentNode] + weight;
-
-      // Update distance if a shorter path is found
-      if (distance < distances[neighborNode]) {
-        distances[neighborNode] = distance;
-        priorityQueue.enqueue({ node: neighborNode, distance });
+  if (depth > 0) {
+    visited.add(node);
+    if (graph[node]) {
+      for (let neighbor of graph[node]) {
+        if (!visited.has(neighbor)) {
+          if (dls(graph, neighbor, target, depth - 1, visited)) {
+            return true;
+          }
+        }
       }
     }
   }
-
-  return distances;
+  return false;
 }
 
+// Example usage
 const graph = {
-  0: { 1: 4, 2: 1 },
-  1: { 3: 1 },
-  2: { 1: 2, 3: 5 },
-  3: {},
+  'A': ['B', 'C'],
+  'B': ['D', 'E'],
+  'C': ['F'],
+  'D': [],
+  'E': ['F'],
+  'F': []
 };
 
-const distances = dijkstra(graph, 0);
+const start = 'A';
+const target = 'F';
+const maxDepth = 3;
 
-for (const node in distances) {
-  console.log(`Distance from 0 to ${node}: ${distances[node]}`);
+if (iddfs(graph, start, target, maxDepth)) {
+  console.log('Path exists');
+} else {
+  console.log('Path does not exist');
 }
